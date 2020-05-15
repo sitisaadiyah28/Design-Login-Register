924c3355c241c5235b6d40951cb3dac546fd7c5e
DELL <sitisaadiyahap16@gmail.com>
2020-05-15 18:43:19 +0700

    Login Register

---
+++ lib/main.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: LoginPage(),
+));
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: ListView(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, top: 50.0),
+              child: Text('LOGIN', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30, color: Colors.pink),),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, top: 60),
+              child: Text(
+                'Username', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+              child: TextFormField(
+
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, top: 20),
+              child: Text(
+                'Password', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20),
+              child: TextFormField(
+                  obscureText: true,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: Center(
+                child: Container(
+                  width: MediaQuery.of(context).size.width/2,
+                  child: RaisedButton(
+                    color: Colors.pink,
+                    onPressed: (){
+
+                    },
+                    child: Text('Login', style: TextStyle(color: Colors.white, fontSize: 18),),
+                  ),
+                ),
+              ),
+            ),
+            FlatButton(
+              onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context) => RegisterPage(
+                )));
+              },
+              child: Text('Register', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: ListView(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, top: 50.0),
+              child: Text('REGISTER', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30, color: Colors.pink),),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, top: 60),
+              child: Text(
+                'Username', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+              child: TextFormField(
+
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, top: 20),
+              child: Text(
+                'Alamat', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+              child: TextFormField(
+
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, top: 20),
+              child: Text(
+                'No Telp', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+              child: TextFormField(
+
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, top: 20),
+              child: Text(
+                'Password', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20),
+              child: TextFormField(
+                obscureText: true,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: Center(
+                child: Container(
+                  width: MediaQuery.of(context).size.width/2,
+                  child: RaisedButton(
+                    color: Colors.pink,
+                    onPressed: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage(
+                      )));
+                    },
+                    child: Text('Register', style: TextStyle(color: Colors.white, fontSize: 18),),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
